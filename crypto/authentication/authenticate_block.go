8f032691700ae39533a6570412dcfb5921d10900
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-09-14 12:18:50 +0100

    Slight refactoring of the code and adding in a public key for a known node

--- crypto/authentication/authenticate_block.go
+++ crypto/authentication/authenticate_block.go
@@ -1,7 +1,6 @@
 package authentication
 
 import (
-	"crypto/ecdsa"
 	"github.com/pkg/errors"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -26,6 +25,8 @@ func AuthenticateBlock(block *types.Block, am *accounts.Manager, coinbase *commo
 	hash := crypto.Keccak256(encodedNonce[:])
 	sig, err := localKeystore.SignHashWithPassphrase(accounts.Account{Address: *coinbase} , password, hash)
 
+
+
 	if err != nil {
 		return err
 	}
@@ -39,38 +40,26 @@ func fetchKeystore(am *accounts.Manager) *keystore.KeyStore {
 	return am.Backends(keystore.KeyStoreType)[0].(*keystore.KeyStore)
 }
 
+// Useful SE answer: https://ethereum.stackexchange.com/questions/13778/get-public-key-of-any-ethereum-account/13892
+
 // Verify that the Block must have originated from the holder of the expected private key.
 // Given the public key that is paired with the expected, unknown, private key check that Block must have been signed by
 // the expected private key.
-func VerifyBlockAuthenticity(block *types.Block, publicKey *ecdsa.PublicKey) (bool, error) {
+func VerifyBlockAuthenticity(block *types.Block) (bool, error) {
 	if block == nil || block.Header() == nil || block.ExtendedHeader() == nil {
 		return false, errors.New("The Block is not correctly formatted: The Block, it's header and the extended header should not be nil")
 	}
 
 	// Extract from the signature the public key that is paired with the private key; that was used to sign the block
-	pub, err := crypto.SigToPub(createHash(block.Header().Nonce[:]), *block.ExtendedHeader())
+	publicKey, err := crypto.SigToPub(createHash(block.Header().Nonce[:]), *block.ExtendedHeader())
 	if err != nil {
 		return false, err
 	}
-	return matchesExpectedPublicKey(pub, publicKey)
+
+	return IsMinerInWhitelist(publicKey), nil
 }
 
 // Create a hash of the 'message' - to ensure that it is the correct length for the ECDSA algorithm.
 func createHash(msg []byte) []byte {
 	return crypto.Keccak256(msg)
-}
-
-// Check that the private key extracted from the signature matches the expected public key
-func matchesExpectedPublicKey(publicKey *ecdsa.PublicKey, expectedPublicKey *ecdsa.PublicKey) (matches bool, err error) {
-	if publicKey == nil || expectedPublicKey == nil {
-		return false, errors.New("Neither of the public keys to compare should be nil")
-	}
-
-	// Verify that both of the private keys are on the same Elliptic curve
-	if publicKey.Curve != expectedPublicKey.Curve {
-		return false, nil
-	}
-
-	// Check that the X and Y coordinates of the two public keys match exactly
-	return expectedPublicKey.Y.Cmp(publicKey.Y) == 0 && expectedPublicKey.X.Cmp(publicKey.X) == 0, nil
 }
--- crypto/authentication/authorised_miners.go
+++ crypto/authentication/authorised_miners.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"math/big"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pkg/errors"
 )
 
 type AuthorisedMiner *ecdsa.PublicKey
@@ -17,10 +18,18 @@ var ValidMiner1 = ecdsa.PublicKey{
 	big.NewInt(1),
 }
 
+var validMiner2X, _ = new(big.Int).SetString("83627328701153660129122311979087170547012155418906152112136635125509300377318", 10)
+var validMiner2Y, _ = new(big.Int).SetString("108196635521158057672584401627437779732742303084877413837400915130868400893055", 10)
+var ValidMiner2 = ecdsa.PublicKey{
+	crypto.S256(),
+	validMiner2X,
+	validMiner2Y,
+}
+
 func RetrieveAuthorisedMiners() Whitelist {
-	var a AuthorisedMiner = &ValidMiner1
 	cachedWhitelist = []AuthorisedMiner{
-		a,
+		&ValidMiner1,
+		&ValidMiner2,
 	}
 
 	return cachedWhitelist
@@ -41,4 +50,19 @@ func contains(whitelist Whitelist, miner AuthorisedMiner) bool {
 		}
 	}
 	return false
+}
+
+// Check that the private key extracted from the signature matches the expected public key
+func matchesExpectedPublicKey(publicKey *ecdsa.PublicKey, expectedPublicKey *ecdsa.PublicKey) (matches bool, err error) {
+	if publicKey == nil || expectedPublicKey == nil {
+		return false, errors.New("Neither of the public keys to compare should be nil")
+	}
+
+	// Verify that both of the private keys are on the same Elliptic curve
+	if publicKey.Curve != expectedPublicKey.Curve {
+		return false, nil
+	}
+
+	// Check that the X and Y coordinates of the two public keys match exactly
+	return expectedPublicKey.Y.Cmp(publicKey.Y) == 0 && expectedPublicKey.X.Cmp(publicKey.X) == 0, nil
 }
