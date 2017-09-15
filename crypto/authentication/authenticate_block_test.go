6ca6348555f99c384ea690c54d0113da23a46372
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-09-15 12:45:39 +0100

    Adding more tests for the block validation Including a failing test when the nonce is the same and a signature from a previous block is injected

--- crypto/authentication/authenticate_block_test.go
+++ crypto/authentication/authenticate_block_test.go
@@ -10,14 +10,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+	"bytes"
 )
 
 
 const (
 	NONCE_VALUE_1    uint64 = uint64(0xa13a5a8c8f2bb1c4)
+	NONCE_VALUE_2    uint64 = uint64(0x690d71d0195b542d)
+	NONCE_VALUE_3    uint64 = uint64(0xcf7743040301d974)
 	ACCOUNT_PASSWORD string = ""
 	EXTRA_VALUE_1    string = "test block"
 )
+
+var currentBlockNumber int64 = 0
 
 func TestCanAuthenticateBlock(t *testing.T) {
 	dir, ks := createKeystore(t)
@@ -26,7 +32,7 @@ func TestCanAuthenticateBlock(t *testing.T) {
 	account1 := createNewAccount(t, ks)
 
 	am := createAccountManager(ks)
-	block := getBlock(false)
+	block := getNewBlock(NONCE_VALUE_1)
 
 	if block.ExtendedHeader() != nil {
 		t.Error("Expected that the block would not have been signed yet.")
@@ -42,14 +48,16 @@ func TestCanAuthenticateBlock(t *testing.T) {
 	}
 }
 
+// Check to make sure that if a miner that is in the whitelist mines a block, that the verification process succeeds - the block is considered valid
 func TestCanAuthoriseAndVerifyABlock(t *testing.T) {
 	dir, ks := createKeystore(t)
 	defer os.RemoveAll(dir)
 
 	account1 := createNewAccount(t, ks)
+	whitelistMiner(t, account1)
 
 	am := createAccountManager(ks)
-	block := getBlock(false)
+	block := getNewBlock(NONCE_VALUE_2)
 
 	if block.ExtendedHeader() != nil {
 		t.Error("Expected that the block would not have been signed yet.")
@@ -60,31 +68,196 @@ func TestCanAuthoriseAndVerifyABlock(t *testing.T) {
 		t.Errorf("Unable to add the authentication to the block: %v", err)
 	}
 
-	if block.ExtendedHeader() == nil {
-		t.Error("Expected that the block would have a signature.")
-	}
-
-	key, err := getKey(account1.Address, account1.URL.Path)
-	if err != nil {
-		t.Errorf("Unable to retrieve the key from the keystore: %v", err)
-	}
-
-	pubKey := key.PrivateKey.PublicKey
-
-	// Add the miner / signer to the whitelist
-	addMinerToWhitelist(&pubKey)
-
-	valid, err := VerifyBlockAuthenticity(block)
-	if err != nil {
-		t.Errorf("Unable to validate the signature on the block: %v", err)
-	}
+	valid := validateAuthentication(t, block)
 
 	if !valid {
 		t.Error("Expected that the block would have a *valid* signature.")
 	}
 }
 
+// Check to make sure that if a miner that is not in the whitelist mines a block, that the verification process fails - the block is considered invalid
+func TestCanNotVerifyABlockAuthorisedByAMinerNotInTheWhitelist(t *testing.T) {
+	dir, ks := createKeystore(t)
+	defer os.RemoveAll(dir)
 
+	account1 := createNewAccount(t, ks)
+
+	am := createAccountManager(ks)
+	block := getNewBlock(NONCE_VALUE_3)
+
+	if block.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err := AuthenticateBlock(block , am, &account1.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid := validateAuthentication(t, block)
+
+	if valid {
+		t.Error("Expected that the block would *not* have been considered authentic because the miner is not in the whitelist.")
+	}
+}
+
+func TestSignaturesOnSubsequentBlocksAreNotTheSame(t *testing.T) {
+	dir, ks := createKeystore(t)
+	defer os.RemoveAll(dir)
+
+	account1 := createNewAccount(t, ks)
+	account2 := createNewAccount(t, ks)
+	account3 := createNewAccount(t, ks)
+
+	whitelistMiner(t, account1)
+	whitelistMiner(t, account2)
+	whitelistMiner(t, account3)
+
+	am := createAccountManager(ks)
+	block1 := getNewBlock(NONCE_VALUE_1)
+
+	if block1.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err := AuthenticateBlock(block1 , am, &account1.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid := validateAuthentication(t, block1)
+
+	if !valid {
+		t.Error("Expected that the block would have a *valid* signature.")
+	}
+
+	//
+	block2 := getNewBlock(NONCE_VALUE_2)
+
+	if block2.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err = AuthenticateBlock(block2 , am, &account2.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid = validateAuthentication(t, block2)
+
+	if !valid {
+		t.Error("Expected that the block would have a *valid* signature.")
+	}
+
+	//
+	block3 := getNewBlock(NONCE_VALUE_3)
+
+	if block3.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err = AuthenticateBlock(block3 , am, &account3.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid = validateAuthentication(t, block3)
+
+	if !valid {
+		t.Error("Expected that the block would have a *valid* signature.")
+	}
+
+	// Block sanity tests
+
+	// The block numbers must be sequential
+	if block1.Number().Cmp(block2.Number()) >= 0 || block1.Number().Cmp(block3.Number()) >= 0 || block2.Number().Cmp(block3.Number()) >= 0 {
+		t.Error("Expected that the blocks would have different numbers")
+	}
+
+	if block1.ExtendedHeader() == block2.ExtendedHeader() || block1.ExtendedHeader() == block3.ExtendedHeader() || block2.ExtendedHeader() == block3.ExtendedHeader() {
+		t.Error("Expected that the blocks' signatures would be different")
+	}
+}
+
+func TestCanNotInjectValidSignatureIntoANewBlock(t *testing.T) {
+	dir, ks := createKeystore(t)
+	defer os.RemoveAll(dir)
+
+	account1 := createNewAccount(t, ks)
+	whitelistMiner(t, account1)
+
+	am := createAccountManager(ks)
+	block1 := getNewBlock(NONCE_VALUE_1)
+
+	if block1.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err := AuthenticateBlock(block1 , am, &account1.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid := validateAuthentication(t, block1)
+
+	if !valid {
+		t.Error("Expected that the block would have a *valid* signature.")
+	}
+
+	block2 := getNewBlock(NONCE_VALUE_2)
+
+	// Inject valid signature from a previous block
+	block2.SetExtendedHeader(*block1.ExtendedHeader())
+	if bytes.Compare(*block1.ExtendedHeader(), *block2.ExtendedHeader()) != 0 {
+		t.Error("Expected that the signatures on the two block would be the same")
+	}
+
+	valid = validateAuthentication(t, block2)
+
+	if valid {
+		t.Error("Expected that the injected block signature would not be considered valid.")
+	}
+}
+
+func TestCanNotInjectValidSignatureIntoANewBlockWithTheSameNonce(t *testing.T) {
+	dir, ks := createKeystore(t)
+	defer os.RemoveAll(dir)
+
+	account1 := createNewAccount(t, ks)
+	whitelistMiner(t, account1)
+
+	am := createAccountManager(ks)
+	block1 := getNewBlock(NONCE_VALUE_1)
+
+	if block1.ExtendedHeader() != nil {
+		t.Error("Expected that the block would not have been signed yet.")
+	}
+
+	err := AuthenticateBlock(block1 , am, &account1.Address, ACCOUNT_PASSWORD)
+	if err != nil {
+		t.Errorf("Unable to add the authentication to the block: %v", err)
+	}
+
+	valid := validateAuthentication(t, block1)
+
+	if !valid {
+		t.Error("Expected that the block would have a *valid* signature.")
+	}
+
+	block2 := getNewBlock(NONCE_VALUE_1)
+
+	// Inject valid signature from a previous block
+	block2.SetExtendedHeader(*block1.ExtendedHeader())
+	if bytes.Compare(*block1.ExtendedHeader(), *block2.ExtendedHeader()) != 0 {
+		t.Error("Expected that the signatures on the two block would be the same")
+	}
+
+	valid1 := validateAuthentication(t, block1)
+
+	if valid1 {
+		t.Error("Expected that the injected block signature would not be considered valid.")
+	}
+}
 
 // Functions used for setting up the test
 
@@ -112,35 +285,52 @@ func createNewAccount(t *testing.T, ks *keystore.KeyStore) (*accounts.Account) {
 	return &acc
 }
 
-func getBlock(blankNonce bool) *types.Block {
-
-	if blankNonce {
-		return getVanillaBlockNoNonce()
+func getNewBlock(nonceValue uint64) *types.Block {
+	currentBlockNumber++
+	if nonceValue == 0 {
+		return getVanillaBlock(NONCE_VALUE_1)
 	} else {
 		// Create a test block to move around the database and make sure it's really new
-		return getVanillaBlock()
+		return getVanillaBlock(nonceValue)
 	}
 
 
 }
 
-func getVanillaBlock() *types.Block {
+func getVanillaBlock(nonceValue uint64) *types.Block {
 	return types.NewBlockWithHeader(&types.Header{
 		Extra:       []byte(EXTRA_VALUE_1),
 		UncleHash:   types.EmptyUncleHash,
 		TxHash:      types.EmptyRootHash,
 		ReceiptHash: types.EmptyRootHash,
-		Nonce:		 types.EncodeNonce(NONCE_VALUE_1),
+		Number:      big.NewInt(currentBlockNumber),
+		Nonce:		 types.EncodeNonce(nonceValue),
 	})
 }
 
-func getVanillaBlockNoNonce() *types.Block {
-	return types.NewBlockWithHeader(&types.Header{
-		Extra:       []byte(EXTRA_VALUE_1),
-		UncleHash:   types.EmptyUncleHash,
-		TxHash:      types.EmptyRootHash,
-		ReceiptHash: types.EmptyRootHash,
-	})
+func whitelistMiner(t *testing.T, account *accounts.Account) {
+	key, err := getKey(account.Address, account.URL.Path)
+	if err != nil {
+		t.Errorf("Unable to retrieve the key from the keystore: %v", err)
+	}
+
+	pubKey := key.PrivateKey.PublicKey
+
+	// Add the miner / signer to the whitelist
+	addMinerToWhitelist(&pubKey)
+}
+
+func validateAuthentication(t *testing.T, block *types.Block) (bool) {
+	if block.ExtendedHeader() == nil {
+		t.Error("Expected that the block would have a signature.")
+	}
+
+	valid, err := VerifyBlockAuthenticity(block)
+	if err != nil {
+		t.Errorf("Unable to validate the signature on the block: %v", err)
+	}
+
+	return valid
 }
 
 // Duplicate of the code in keystore_plain and keystore_passphrase because there seemed to be no other way to
