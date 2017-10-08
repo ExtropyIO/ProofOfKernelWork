d0371c12cce10383d6ace1b86a6ac386eecf7193
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-10-08 14:47:30 +0100

    Initial draft of adding the 'extended header' into the block header

--- eth/backend.go
+++ eth/backend.go
@@ -166,6 +166,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 	}
 	eth.miner = miner.New(eth, eth.chainConfig, eth.EventMux(), eth.engine)
 	eth.miner.SetExtra(makeExtraData(config.ExtraData))
+	eth.miner.SetAuthentication(ctx.GetDataDir())
 
 	eth.ApiBackend = &EthApiBackend{eth, nil}
 	gpoParams := config.GPO
